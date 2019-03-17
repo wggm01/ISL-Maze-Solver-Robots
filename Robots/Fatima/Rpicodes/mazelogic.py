ea2c8fe4a293fb7bd6385b60d1dd1552b768678b
WVALDO GRAELL <wvaldo.graell@utp.ac.pa>
2019-03-17 14:21:30 -0500

    Codigo unido y listo para pruebas

---
+++ Robots/Fatima/Rpicodes/mazelogic.py
@@ -0,0 +1,193 @@
+
+import time
+#######CCW##########
+regC=[0,0,0,0] #Incrementador para ir guardando distancias
+pnt0=[0]*47 #5-45
+pnt1=[0]*47 #46-90
+pnt2=[0]*47 #90-135
+pnt3=[0]*47 #136-180
+average=[0,0,0,0]#sumatorias
+movlog=[0,0,0,0] #decide el movimiento
+
+#######CW##########
+regC_cw=[0,0,0,0] #Incrementador para ir guardando distancias
+pnt0_cw=[0]*47 #5-45
+pnt1_Cw=[0]*47 #46-90
+pnt2_cw=[0]*47 #90-135
+pnt3_cw=[0]*47 #136-180
+average_cw=[0,0,0,0]#sumatorias
+movlog_cw=[0,0,0,0] #decide el movimiento
+#################
+idle=[0,0]#indicador de proceso completo
+rmov=[0,0] #redundancia
+#################
+
+def logic(dire,theta,r):
+    if dire == "ccw":
+        if (theta < 46): #check
+            # cantidad de puntos [para realizar el promedio]
+            pnt0[regC[0]] = r
+            regC[0] += 1  # Realiza el incremento para ir guardando las r en pnt
+            # promedio de distancia[max dist 38cm]
+            if theta == 45:
+                average[0] = sum(pnt0) / len(pnt0)
+                if average[0] < 40:
+                    movlog[0] = 1  # instruccion logica para movimiento de robot
+                    regC[0] = 0  # reincia contador para nuevo analisis
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog[0] = 0
+                    regC[0] = 0  # reincia contador para nuevo analisis
+
+
+        elif (theta > 45 and theta < 91):  # check
+            # cantidad de puntos [para realizar el promedio]
+            pnt1[regC[1]] = r
+            regC[1] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 90:
+                average[1] = sum(pnt1) / len(pnt1)
+                if average[1] < 40:
+                    movlog[1] = 1  # instruccion logica para movimiento de robot
+                    regC[1] = 0  # reincia contador para nuevo analisis
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog[1] = 0
+                    regC[1] = 0
+
+
+        elif (theta > 90 and theta < 136):  # check
+            # cantidad de puntos [para realizar el promedio]
+            pnt2[regC[2]] = r
+            regC[2] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 135:
+                average[2] = sum(pnt2) / len(pnt2)
+                if average[2] < 40:
+                    movlog[2] = 1
+                    regC[2] = 0
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog[2] = 0
+                    regC[2] = 0
+
+        elif (theta > 135 and theta < 181):  # check
+            # cantidad de puntos [para realizar el promedio]
+            pnt3[regC[3]] = r
+            regC[3] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 180:
+                average[3] = sum(pnt3) / len(pnt3)
+                if average[3] < 40:
+                    movlog[3] = 1
+                    regC[3] = 0
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog[3] = 0
+                    regC[3] = 0
+
+        elif theta == 180:
+            # decision de movimiento:
+            rmov[0] = str(movlog[0]) + str(movlog[1]) + str(movlog[2]) + str(movlog[3])
+            print(rmov[0])
+            idle[0] = 1  # identificador de que se completo el analisis en cw
+
+
+############################################################                        
+    elif dire == "cw":
+        if (theta > 170 and theta < 136):  # check
+            # cantidad de puntos [para realizar el promedio]
+            pnt0_cw[regC_cw[0]] = r
+            regC_cw[0] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 135:
+                average_cw[0] = sum(pnt0_cw) / len(pnt0_cw)
+                if average_cw[0] < 40:
+                    movlog_cw[3] = 1
+                    regC_cw[0] = 0
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog_cw[3] = 0
+                    regC_cw[0] = 0
+
+        elif (theta > 134 and theta < 91): #check
+            # cantidad de puntos [para realizar el promedio]
+            pnt1_cw[regC_cw[1]] = r
+            regC_cw[1] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 90:
+                average_cw[1]=sum(pnt1_Cw)/len(pnt1_Cw)
+                if average_cw[1] <40:
+                    movlog_cw[2] = 1
+                    regC_cw[1] = 1
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog_cw[2] = 0
+                    regC_cw[1] = 1
+
+        elif (theta > 89 and theta < 46): #check
+            # cantidad de puntos [para realizar el promedio]
+            pnt2[regC_cw[2]] = r
+            regC_cw[2] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 45:
+               average_cw[2]=sum(pnt2_cw)/len(pnt2_cw)
+                if average_cw[2] <40:
+                    movlog_cw[1] = 1
+                    regC_cw[2] = 0
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog_cw[1] = 0
+                    regC_cw[2] = 0
+
+        elif (theta < 46): #check
+            # cantidad de puntos [para realizar el promedio]
+            pnt3[regC_cw[3]] = r
+            regC_cw[3] += 1
+            # promedio de distancia[max dist 38cm]
+            if theta == 180:
+                average_cw[3]=sum(pnt3_cw)/len(pnt3_cw)
+                if prom < 40:
+                    movlog_cw[0] = 1
+                    regC_cw[3] = 0
+                else:
+                    # obstaculo logico[1=pared, 0=libre]
+                    movlog_cw[0] = 0
+                    regC_cw[3] = 0
+
+        elif theta == 0:
+            # decision de movimiento:
+            rmov[1] = str(movlog[3]) + str(movlog[2]) + str(movlog[1]) + str(movlog[0])
+            idle[1] = 1
+
+    elif idle[0] * idle[1] == 1:
+        print(rmov[0] + rmov[1])
+        if rmov[0] == rmov[1]:
+            if mov == "1001":
+                # avanzar recto
+                idle[0] = 0
+                idle[1] = 0
+                return 'w'
+            if mov == "0001":
+                # avanzar izquierda 90 grados
+                idle[0] = 0
+                idle[1] = 0
+                return 'a'
+            if mov == "1000":
+                # avanzar derecha 90 grados
+                idle[0] = 0
+                idle[1] = 0
+                return 'd'
+            if mov == "1111":
+                # rotar 180
+                return 'q'
+                idle[0] = 0
+                idle[1] = 0
+
+        else:
+            print("mapeando")
+
+                    
+
+######################################
+
--- Robots/Fatima/Rpicodes/rpiard.py
+++ Robots/Fatima/Rpicodes/rpiard.py
@@ -1,6 +1,6 @@
-import mazedist
-from mazedist import logic
-from mazedist import awd
+
+from mazelogic import logic # se encarga de la logica en el modo maze(opcion de debugger usando procesing)
+from locomo import mov # se encarga de los movimientos en el modo manual
 import RPi.GPIO as GPIO
 import sys
 import serial
@@ -15,15 +15,151 @@ GPIO.setup(ledS,GPIO.OUT)
 GPIO.setup(nmos,GPIO.OUT) 
 GPIO.setup(b1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) #la raspberry actuara cuando reciba 3.3v
 GPIO.setup(b2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-flag=0
-b1F=0
-b2F=0
-enaS=1
+####INCIALIZACION DE PINES PARA L298N####
+motorA1=4 #out1 in1
+motorA2=23 #out1 in3
+motorB1=17 #out2 in2
+motorB2=22 #out2 in4
+motorena=18
+motorenb=26
+GPIO.setwarnings(False)
+GPIO.setup(motorA1,GPIO.OUT)
+GPIO.setup(motorA2,GPIO.OUT)
+GPIO.setup(motorB1,GPIO.OUT)
+GPIO.setup(motorB2,GPIO.OUT)
+GPIO.setup(motorena,GPIO.OUT)
+GPIO.setup(motorenb,GPIO.OUT)
+ena=GPIO.PWM(motorena,500)
+enb=GPIO.PWM(motorenb,500)
+ena.start(0)
+enb.start(0)
+####INCIALIZACION DE PINES PARA L298N####
+###CONTROL DE INCIIALIZACION###
+flag=0 #activa la secuencia de activacion y acepta el modo debug
+flag1=0 #me ayuda a negar el modo debug solamente
+b1F=0 #elige el modo maze
+b2F=0 #elige el modo manual
+enaS=1 # Flag para correr codigo una sola vez
+ena2S=1 #Flag para correr codigo una sola vez
+debug =0 #control de modo debug
+###CONTROL DE INCIIALIZACION###
+
+###tcp###
+imu=[0,0,0] # yaw, pitch, roll
+rad=['s',0,0] #'dire','ang','distance'
+HOST= '192.168.25.113'
+PORT= 65441
+###tcp###
+
+######Movimiento de los motores######
+def adelante(mode):
+    ena.ChangeDutyCycle(90)  # duty cycle
+    enb.ChangeDutyCycle(90)
+    GPIO.output(motorA1, GPIO.LOW)
+    GPIO.output(motorA2, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.HIGH)
+    GPIO.output(motorB2, GPIO.HIGH)
+    if mode == 1:
+        time.sleep(1.5)
+        detenerse()
+
+
+def izquierda(mode):
+    ena.ChangeDutyCycle(75)
+    GPIO.output(motorA1, GPIO.LOW)
+    GPIO.output(motorA2, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.HIGH)
+    GPIO.output(motorB2, GPIO.LOW)
+    if mode == 1:
+        time.sleep(1.5)
+        detenerse()
+
+
+def spinizq(mode):
+    ena.ChangeDutyCycle(75)
+    enb.ChangeDutyCycle(75)
+    GPIO.output(motorA1, GPIO.LOW)
+    GPIO.output(motorA2, GPIO.HIGH)
+    GPIO.output(motorB1, GPIO.HIGH)
+    GPIO.output(motorB2, GPIO.LOW)
+    if mode == 1:
+        time.sleep(1.5)
+        detenerse()
+
+
+def derecha(mode):
+    enb.ChangeDutyCycle(75)
+    GPIO.output(motorA1, GPIO.LOW)
+    GPIO.output(motorA2, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.LOW)
+    GPIO.output(motorB2, GPIO.HIGH)
+    if mode == 1:
+        time.sleep(1.5)
+        detenerse()
+
+
+def spinder(mode):
+    enb.ChangeDutyCycle(75)
+    ena.ChangeDutyCycle(75)
+    GPIO.output(motorA1, GPIO.HIGH)
+    GPIO.output(motorA2, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.LOW)
+    GPIO.output(motorB2, GPIO.HIGH)
+    if mode == 1:
+        time.sleep(1.5)
+        detenerse()
+
+
+def detenerse():
+    ena.ChangeDutyCycle(0)
+    enb.ChangeDutyCycle(0)
+    GPIO.output(motorA1, GPIO.LOW)
+    GPIO.output(motorA2, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.LOW)
+    GPIO.output(motorB1, GPIO.LOW)
+######Movimiento de los motores######
+
+###control manual###
+def mov():
+    data = conn.recv(1024)
+    if not data:
+        print("Modulos Desactivados")
+        conn.close()
+        time.sleep(2)
+        GPIO.cleanup()
+        sys.exit()
+
+    # ANALISIS DE DATA
+    cmdRaw = data.partition('s') # Separa la trama al encontrar el caracter s (cmd,s,'')
+    cmd= cmdRaw[0] # (cmd)
+    # envase al comando realizar un movimiento
+    if (cmd == 'w'):
+        adelante(0)
+    if (cmd == 'a'):
+        izquierda(0)
+    if (cmd == 'd'):
+        derecha(0)
+    if (cmd == 'q'):
+        spinizq(0)
+    if (cmd == 'e'):
+        spinder(0)
+    if (cmd == 'x'):
+        print("Modulos Desactivados")
+        conn.close()
+        time.sleep(2)
+        GPIO.cleanup()
+        sys.exit()
+    else:
+        detenerse() #se enviara una z indicando que el boton ha sido soltado.
+###control manual###
+
+###incio del programa###
 GPIO.output(nmos,GPIO.LOW)
 time.sleep(2)
 GPIO.output(nmos,GPIO.HIGH)
-ardS = serial.Serial("/dev/serial0", baudrate = 115200)
-#ardS = serial.Serial("/dev/ttyUSB0", baudrate = 115200)
+#ardS = serial.Serial("/dev/serial0", baudrate = 115200)
+ardS = serial.Serial("/dev/ttyUSB0", baudrate = 115200)
+
 try:
     #Prende Led de estado y espera lectura de boton.
     GPIO.output(ledS,GPIO.HIGH)
@@ -77,36 +213,172 @@ try:
                 b2F=2
                 print(b2F)
                 break #SALE DEL WHILE SI ALGUNO DE LOS DOS ES PRESIONADO
-
-    while b1F == 1:
+####################MODO MAZE###################################
+    while b1F == 1: #Modo maze activo
         print("MODO MAZE")
-        if enaS == 1:
-            ardS.write(b"R") #Estas despierto?
-            ardS.write(b"s") #Estas despierto?
+        # control de modo debug:
+        while (flag == 1):
+            for i in range(2):
+                GPIO.output(ledS, GPIO.HIGH)
+                time.sleep(1)  # parpadea 2 veces indica eleccion de debug o !debug.
+                GPIO.output(ledS, GPIO.LOW)
+            print("desea activar modo maze con debug o !debug")
+            b1S = GPIO.input(b1)  # lectura de estado de botones
+            b2S = GPIO.input(b2)
             time.sleep(0.5)
-            ardS.write(b"I") #Estas despierto?
-            ardS.write(b"s") #Estas despierto?
-            enaS=0
-        #ardR=ardS.readline()
-        #print (ardR)
-        #logic(dire,theta,r)
-        #awd(gyrox,gyroy,gyroz,accx,accy,accz,magx,magy,magz)
+            if (b1S == 1):  # debug
+                flag = 0
+                debug 1
+            ######TCP INICIALIZACION#####
+                print("Creando Socket")
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                except socket.error:
+                    print('Error Socket')
+                    print("Modulos Desactivados")
+                    ardS.write(b'E')
+                    ardS.write(b's')
+                    time.sleep(2)
+                    sys.exit()
+                s.bind((HOST, PORT))
+                s.listen(3)  # Maximo tres clientes
+                conn, addr = s.accept()
+                print('Connected by', addr)
+            ######TCP INICIALIZACION#####
+                break
+            if (b2S == 1):  # !debug
+                flag = 0
+                debug =0
+                break
+
+        if enaS == 1:  # una sola ejecucion
+            enaS = 0
+            for i in range(3):  # parpadeo de led de estado 3 veces indica modo maze.
+                GPIO.output(ledS, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(ledS, GPIO.LOW)
+
+            if (debug):
+                ######ARDUINO ACTIVACION DE SISTEMA#####
+                for i in range(0, 1):
+                    ardS.write(b'R')  # Activacion de los sensores
+                    time.sleep(1)
+                    ardS.write(b's')  # fin de mensaje
+                    time.sleep(0.5)
+                    print("Intentando conectar")
+                print("UART establecido")
+                ######ARDUINO ACTIVACION DE SISTEMA#####
+            else:
+                ######ARDUINO ACTIVACION DE SISTEMA#####
+                for i in range(0, 1):
+                    ardS.write(b'R')  # Activacion de los sensores
+                    time.sleep(1)
+                    ardS.write(b's')  # fin de mensaje
+                    time.sleep(0.5)
+                    print("Intentando conectar")
+                print("UART establecido")
+            ######ARDUINO ACTIVACION DE SISTEMA#####
+
+        ###ANALISIS DE DATA PROVENIENETE DEL ARDUINO###
+        data_raw = ardS.readline()
+        if data_raw:
+            datastr = data_raw.decode("utf-8")
+            dsplit = datastr.split(",")
+            if (dsplit[0] == 'I'):
+                # print(dsplit)
+                imu[0] = float(dsplit[1])
+                imu[1] = float(dsplit[2])
+                imu[2] = float(dsplit[3])
+                # imu[3]=imu[2].rstrip('\r\n')
+                # imu[4]=float(imu[2])
+                continue
+            if (dsplit[0] == 'R'):
+                rad[0] = dsplit[1]
+                rad[1] = float(dsplit[2])
+                rad[2] = dsplit[3]
+                rad[2] = rad[2].rstrip('\n')
+                rad[2] = float(rad[2])
+                continue
+        else:
+            print("no data")
+            print('No se pudo enviar la informacion')
+            print("Modulos Desactivados")
+            ardS.write(b'E')
+            ardS.write(b's')
+            if (debug):
+                conn.close()
+            time.sleep(2)
+            sys.exit()
+        ########LOGICA DE MAZE########
+        cmd = mazelogic.logic(rad[0], rad[1], rad[2])
+        if (cmd == 'w'):
+            adelante(1)
+        if (cmd == 'a'):
+            izquierda(1)
+        if (cmd == 'd'):
+            derecha(1)
+        if (cmd == 'q'):
+            spinizq(1)
+        if (cmd == 'e'):
+            spinder(1)
+        else:
+            detenerse()
+        ########LOGICA DE MAZE########
+
+        if (debug):
+            data = str(rad[1]) + ',' + str(rad[2]) + ',' + str(imu[0]) + ',' + str(imu[1]) + ',' + str(imu[2]) + ',' + 's'
+            try:
+                conn.sendall(data)
+            except socket.error:
+                print('No se pudo enviar la informacion')
+                print("Modulos Desactivados")
+                ardS.write(b'E')
+                ardS.write(b's')
+                if (debug):
+                    conn.close()
+
+                time.sleep(2)
+                sys.exit()
+
+        ###ANALISIS DE DATA PROVENIENETE DEL ARDUINO###
+
         b1S=GPIO.input(b1)
         if (b1S == 1):
             print ("Modulos Desactivados")
-            ardS.write(b"E") #Estas despierto?
-            ardS.write(b"s") #Estas despierto?
+            if (debug):
+                conn.close()
+            ardS.write(b"E") #Desactiva los sensores
+            ardS.write(b"s")
             time.sleep(2)
             GPIO.cleanup()
             sys.exit()
-            
-    while b2F == 2:
-        #Modo manual por definir
+ #########################MODO MANUAL###############################################
+    while b2F == 2: #Modo manual activo
+        if ena2S = 1:
+            ena2S=0
+            for i in range(5): #parpadeo 5 veces modo manual
+                GPIO.output(ledS, GPIO.HIGH)
+                time.sleep(1)  # parpadeo indica modo manual
+                GPIO.output(ledS, GPIO.LOW)
+
+        ####conexion TCP ######
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        conn, addr = s.accept()
+        print('Connected by', addr)
+        ####conexion TCP ######
+        mov()
+
+
         b1S=GPIO.input(b1)
         if (b1S == 1):
             print ("Modulos Desactivados")
-            ardS.write(b"E") #Estas despierto?
-            ardS.write(b"s") #Estas despierto?
+            if (debug):
+                conn.close()
+            ardS.write(b"E") #Desactiva los sensores
+            ardS.write(b"s")
             time.sleep(2)
             GPIO.cleanup()
             sys.exit()
@@ -114,6 +386,8 @@ try:
 
 except KeyboardInterrupt :
     print ("Modulos Desactivados")
+    if (debug):
+        conn.close()
     ardS.write(b"E") 
     ardS.write(b"s") 
     time.sleep(2)
